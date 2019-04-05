3a4baff494bc94418538333dcedb5291c0bdce14
Mark Gardner <mark.gardner@twobitcircus.com>
2019-04-05 15:05:13 -0700

    Initial code commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/runeimp/nosjog
 
 go 1.12
+
+require github.com/tidwall/pretty v0.0.0-20190325153808-1166b9ac2b65
---
+++ nosjog.go
@@ -0,0 +1,60 @@
+package nosjog
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"github.com/tidwall/pretty"
+)
+
+/*
+ * CONSTANTS
+ */
+const (
+	Name    = "NOSJoG"
+	Version = "0.1.0"
+)
+
+// Dynamic converts JSON bytes to an interface{} object
+func Dynamic(jsonBytes []byte) interface{} {
+	var anything interface{}
+	json.Unmarshal(jsonBytes, &anything)
+	return anything
+}
+
+// Marshal encodes objects without HTML escaping the data
+func Marshal(t interface{}) ([]byte, error) {
+	buffer := &bytes.Buffer{}
+	encoder := json.NewEncoder(buffer)
+	encoder.SetEscapeHTML(false)
+	err := encoder.Encode(t)
+	return buffer.Bytes(), err
+}
+
+// PrettyJSON converts JSON bytes to a JSON string
+func PrettyJSON(jsonBytes []byte) string {
+	buffer := new(bytes.Buffer)
+	json.Indent(buffer, jsonBytes, "", "  ")
+	return fmt.Sprintf("%s", buffer)
+}
+
+// Pretty formats JSON bytes to be pretty
+func Pretty(jsonBytes []byte) []byte {
+	prettyOptions := pretty.DefaultOptions
+	prettyOptions.SortKeys = true
+	return pretty.PrettyOptions(jsonBytes, prettyOptions)
+}
+
+// Println takes an object and converts it to JSON bytes then colorizes it for terminal output before printing
+func Println(content interface{}) {
+	jsonBytes, _ := Marshal(content)
+	jsonString := Terminal(jsonBytes)
+	fmt.Println(string(jsonString))
+}
+
+// Terminal colorizes JSON bytes with terminal escape codes
+func Terminal(jsonBytes []byte) []byte {
+	prettyJSON := Pretty(jsonBytes)
+	return pretty.Color(prettyJSON, nil)
+}
